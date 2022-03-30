d8423952e19ea9e9a81ff2b28e2f89ea7fe46ed1
Juan Diego Velarde <jdvelrod@gmail.com>
2022-03-30 13:01:38 +0200

    B016 Map

---
+++ Data structures/B016_Create_a_map.js
@@ -0,0 +1,32 @@
+var Map = function () {
+  this.collection = {};
+  // Only change code below this line
+  this.add = function (key, value) {
+    this.collection[key] = value;
+  };
+
+  this.remove = function (key) {
+    delete this.collection[key];
+  };
+
+  this.get = function (key) {
+    return this.collection[key];
+  };
+
+  this.has = function (key) {
+    return this.collection.hasOwnProperty(key);
+  };
+
+  this.values = function () {
+    return Object.values(this.collection);
+  };
+
+  this.size = function () {
+    return Object.values(this.collection).length;
+  };
+
+  this.clear = function () {
+    this.collection = {};
+  };
+  // Only change code above this line
+};
